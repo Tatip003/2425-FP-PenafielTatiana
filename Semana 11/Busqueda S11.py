91c21536ea30a9506a6eb2d1dff954943485966d
Tatip003 <ts.penafielm@uea.edu.ec>
2025-02-28 14:44:20 -0500

    Tarea semana 11

---
+++ Semana 11/Busqueda S11.py
@@ -0,0 +1,28 @@
+# Búsqueda en Arreglo Multidimensional
+matriz = [
+    [30, 10, 15]
+    [25, 35, 5]
+    [40, 20, 45]
+]
+# Busqueda de valor especifico
+valor_buscado = 5
+
+fila_encontrada = -1
+columna_encontrada =-1
+
+#Recorrer
+for fila in range(len(matriz)):
+     for columna in range (len(matriz[fila])):
+         if matriz [fila] [columna] == valor_buscado:
+             fila_encontrada = fila
+             columna_encontrada = columna
+
+              break
+        if fila_encontrada != -1:
+               break
+
+# Busqueda valor especifico 3n la matriz
+if fila_encontrada != -1:
+    printf(f"Se encontró {valor_buscado} en la fila {fila_encontrada} y columna {columna_encontrada}" )
+else :
+    Printf(f"{valor_buscado} no se encontró en la matriz" )
